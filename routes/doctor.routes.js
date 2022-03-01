00fcd9b9ef900feef9373c3e438bd7f9c0c3ae58
MohamedAmine-BenSalah <mohamedaminebensalah6@gmail.com>
2022-03-01 18:30:54 +0100

    pulling

--- routes/doctor.routes.js
+++ routes/doctor.routes.js
@@ -4,6 +4,7 @@ const doctorController = require("../controllers/doctor.controller");
 router.get("/", doctorController.selectAll);
 router.post("/signup", doctorController.signup);
 router.post("/login", doctorController.login);
-
+router.post("/api/postBlogs", doctorController.insertBlogs);
+router.get("/api/selectBlogs", doctorController.selectBlogs);
 
 module.exports = router;
