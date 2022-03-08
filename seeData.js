29086dfdf46f501b3efa1d78b6ef5b7d43ed14de
MohamedAmine-BenSalah <mohamedaminebensalah6@gmail.com>
2022-03-08 17:33:26 +0100

    pulling

---
+++ seeData.js
@@ -0,0 +1,52 @@
+const data = [
+    {
+        name: "aderian",
+        dateOfBirth: "20/02/2001",
+        dateOfAp: "march 24, 2022 8:00:00 ",
+    },
+    {
+        name: "slim",
+        dateOfBirth: "15/01/2002",
+        dateOfAp: "march 24, 2022 8:35:00 ",
+    },
+    {
+        name: "shady",
+        dateOfBirth: "6/04/2008",
+        dateOfAp: "march 24, 2022 11:00:00 ",
+    },
+    {
+        name: "lora",
+        dateOfBirth: "02/02/2000",
+        dateOfAp: "march 24, 2022 12:00:00 ",
+    },
+    {
+        name: "chad",
+        dateOfBirth: "17/02/2001",
+        dateOfAp: "march 24, 2022 9:00:00 ",
+        
+    },
+    {
+        name: "عبد الرحيم",
+        dateOfBirth: "17/02/2001",
+        dateOfAp: "march 24, 2022 11:35:00 ",
+    },
+    {
+        name: "باديس",
+        dateOfBirth: "17/02/1980",
+        dateOfAp: "march 24, 2022 14:15:00 ",
+    },
+    {
+        name: "عبد الصمد",
+        dateOfBirth: "10/02/1945",
+        dateOfAp: "march 24, 2022 15:00:00 ",
+    },
+    {
+        name: "أم كلثوم",
+        dateOfBirth: '30/05/1910',
+        dateOfAp: "march 24, 2022 16:30:00 ",
+    },
+];
+
+
+
+
